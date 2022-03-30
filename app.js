dd634ec3ccb3cf7792a466d16bb494e0df747519
hrishgore11 <hrishgore11@gmail.com>
2022-03-31 01:09:35 +0530

    express_ejs_Users

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5015;
 const User = require("./Models/User");
-app.set("view engine", "ejs");
 
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
 app.get("/home", (req, res) => {
   User.find().then((data) => {
-    res.render("temp", { data: data, heading: "Users" });
+    res.render("temp", { data: data, heading: "Users :" });
   });
 });
 
@@ -24,12 +24,73 @@ app.post("/api/create", (req, res) => {
   });
 
   user.save().then((data) => {
-    res.json({ message: "User created Sucessfully", data: data });
+    res.redirect("/home");
   });
 });
 //KCXm5qjxW1a7yY2S
+//////////////////////////////////////////////
+app.get("/edit/:_id", (req, res) => {
+  const id = req.params._id;
+
+  User.findById(id, (err, data) => {
+    if (err) {
+      res.redirect("/");
+    } else {
+      if (data == null) {
+        res.redirect("/");
+      } else {
+        res.render("edit", { title: "edit_user", data: data });
+      }
+    }
+  });
+});
+/////////////////////////////////////////////
+app.get("/Add", (req, res) => {
+  res.render("Add");
+});
+/////////////////////////////////////////////
+
+app.post("/api/update/:_id", (req, res) => {
+  const id = req.params._id;
+  const body = req.body;
+
+  User.updateOne(
+    { _id: id },
+    {
+      $set: {
+        name: body.name,
+        mobile: body.mobile,
+        age: body.age,
+        place: body.place,
+        active: body.active,
+      },
+    }
+  )
+    .then(() => {
+      console.log(req.body);
+
+      res.redirect("/home");
+    })
+    .catch((err) => {
+      res.json({ message: "Error", data: err });
+    });
+});
+/////////////////////////////////////////////
+app.get("/delete/:_id", (req, res) => {
+  const id = req.params._id;
+
+  User.deleteOne({ _id: id })
+    .then(() => {
+      res.redirect("/home");
+    })
+    .catch((err) => {
+      res.json({ message: "Error", data: err });
+    });
+});
+/////////////////////////////////////////////
+
 const mongoose = require("mongoose");
-// const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser")
 mongoose
   .connect(
     "mongodb+srv://Hrishi:KCXm5qjxW1a7yY2S@cluster0.1nqxb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
